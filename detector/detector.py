f1eaa360a3d29f12a21e1dea88fa508582c52f76
GeovaniNieswald <geovaninieswald@gmail.com>
2021-04-25 19:49:52 -0300

    Adicionando dataset e normalizando dados

---
+++ detector/detector.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import re
+import multiprocessing
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+from gensim.models import Word2Vec
+
+# TOTAL DE NOTÍCIAS (6335)
+#  FAKE (3164)
+#  REAL (3171)
+
+# DADOS TREINAMENTO (3168)
+#  FAKE (1582)
+#  REAL (1586)
+
+# DADOS DE DETECÇÃO (3167)
+#  FAKE (1582)
+#  REAL (1585)
+
+# Normalizar dados com NLTK
+def normalizar(dados):
+    all_words = []
+
+    # remove coluna referente ao código
+    dados = dados.drop("Unnamed: 0", axis = 1)
+    dados = dados.drop("Unnamed: 0.1", axis = 1)
+
+    # remove linhas que tenham alguma coluna em branco
+    dados.replace("", float("NaN"), inplace=True)
+    dados.dropna(subset = ["title", "text", "label"], inplace=True)
+
+    # colocar tudo em minusculo
+    dados["title"] = dados["title"].str.lower()
+    dados["text"]  = dados["text"].str.lower()
+
+    # remove pontuação e caracteres especiais
+    dados['title'] = dados['title'].apply((lambda x: re.sub('[^A-Za-z0-9 ]+', '', x)))
+    dados['text'] = dados['text'].apply((lambda x: re.sub('[^A-Za-z0-9 ]+', '', x)))
+
+    # remove stopwords e faz a "tokenização"
+    stop_words = set(stopwords.words('english')) 
+    for i, text in enumerate(dados['text'].values):
+        text_tokens = word_tokenize(text)
+        tokens_without_sw = [word for word in text_tokens if not word in stop_words]
+        dados.iat[i, 1] = tokens_without_sw
+
+        all_words.append(tokens_without_sw)
+
+    return dados, all_words
+
+# Pré-processamento com Word Embedding (Word2Vec)
+def pre_processamento(all_words, dados):
+    w2v = Word2Vec(all_words, window = 5, min_count = 5, negative = 15, workers = multiprocessing.cpu_count())
+   
+    # continuar...
+
+    return dados
+
+# -------------------------------------------------------------------------
+
+dados = pd.read_csv('./dataset/news_treinamento.csv')
+dados, all_words = normalizar(dados)
+
+dados_processados = pre_processamento(all_words, dados)
+
+print(dados_processados)
