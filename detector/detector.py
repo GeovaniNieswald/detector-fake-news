aeddd0e110e76d8a87f1c7b3620889e1b4068229
GeovaniNieswald <geovaninieswald@gmail.com>
2021-05-24 15:26:48 -0300

    Realizando pré processamento com Word2Vec

--- detector/detector.py
+++ detector/detector.py
@@ -7,6 +7,8 @@ from nltk.tokenize import word_tokenize
 
 from gensim.models import Word2Vec
 
+teste = True
+
 # TOTAL DE NOTÍCIAS (6335)
 #  FAKE (3164)
 #  REAL (3171)
@@ -52,15 +54,32 @@ def normalizar(dados):
 
 # Pré-processamento com Word Embedding (Word2Vec)
 def pre_processamento(all_words, dados):
-    w2v = Word2Vec(all_words, window = 5, min_count = 5, negative = 15, workers = multiprocessing.cpu_count())
-   
-    # continuar...
+    if teste:
+        w2v = Word2Vec(all_words, window = 5, min_count = 5, negative = 15, workers = multiprocessing.cpu_count())
+        w2v.save("models/word2vec.model")
+        w2v.save("models/model.bin")
+    else:
+        w2v = Word2Vec.load('models/model.bin')
+
+    for i, text in enumerate(dados['text'].values):
+        for j, word in enumerate(text):
+            try:
+                similar = w2v.wv.most_similar(positive=[word], topn = 1)
+
+                if similar[0][1] >= 0.9:
+                    text[j] = [word, similar[0][0]]
+                else:
+                    text[j] = [word]                
+            except KeyError:
+                text[j] = [word]
+
+        dados.iat[i, 1] = text 
 
     return dados
 
 # -------------------------------------------------------------------------
 
-dados = pd.read_csv('./dataset/news_treinamento.csv')
+dados = pd.read_csv('./dataset/news_treinamento_p.csv')
 dados, all_words = normalizar(dados)
 
 dados_processados = pre_processamento(all_words, dados)
